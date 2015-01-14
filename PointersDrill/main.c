17872016ba84423670e4440519e034a21aa26d7d
Mike Reining <emailmike@gmail.com>
2015-01-14 11:34:05 -0800

    Refactored C Pointers - moved * next to type to make it more clear what is going on

--- PointersDrill/main.c
+++ PointersDrill/main.c
@@ -11,30 +11,46 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     
+    // Demo exercise
+    
+    int x = 2;
+    int y = 4;
+    int *pointerY = &y;
+    printf("Pointer y initial value is %d\n",*pointerY);
+    int *pointerX = &x;
+    int *pointerToPointerX = pointerX;
+    *pointerY = *pointerX;
+    printf("Pointer to Pointer value = %d\n",*pointerToPointerX);
+    printf("Pointer y value is now %d\n",*pointerY);
+    
     int a = 13;
     int b = 5;
     int c = 10;
     
-    int *intPointer1 = &a;
-    int *intPointer2 = &b;
-    int *intPointer3 = &c;
-    int *intPointer4 = &c;
+    // convention, put the star next to the object type to make it clear
+    // that if you want to point at the value you use the star later.
+    // Otherwise you just use the variable name
+    
+    int* pInt1 = &a;
+    int* pInt2 = &b;
+    int* pInt3 = &c;
+    int* pInt4 = &c;
     
     printf("Print initial Int values %d, %d, %d\n",a,b,c);
-    printf("Print Pointer Values %d %d %d %d\n", *intPointer1,*intPointer2,*intPointer3, *intPointer4);
+    printf("Print Pointer Values %d %d %d %d\n", *pInt1,*pInt2,*pInt3,*pInt4);
     
     // Double A via pointer
-    *intPointer1 = *intPointer1 * 2;
-    printf("intPointer1 = %d; a = %d\n",*intPointer1,a);
+    *pInt1 = *pInt1 * 2;
+    printf("intPointer1 = %d; a = %d\n",*pInt1,a);
     // Increment C via pointer
-    *intPointer3 = *intPointer3 + 1;
-    printf("intPointer3 = %d; c = %d\n",*intPointer3,c);
+    *pInt3 = *pInt3 + 1;
+    printf("intPointer3 = %d; c = %d\n",*pInt3,c);
     // Point one of C pointers to B via B pointer HELP!  Why does this not work?
-    *intPointer4 = *intPointer2;
-    printf("intPointer4 = %d; b = %d\n",*intPointer4,b);
+    pInt4 = pInt2;
+    printf("intPointer4 = %d; b = %d\n",*pInt4,b);
     // Add 4 to b via updated Pointer
-    *intPointer4 = *intPointer4 + 4;
-    printf("intPointer4 = %d; b = %d\n",*intPointer4,b);
+    *pInt4 = *pInt4 + 4;
+    printf("intPointer4 = %d; b = %d\n",*pInt4,b);
 
     printf("Print initial values after updates %d, %d, %d\n",a,b,c);
 
